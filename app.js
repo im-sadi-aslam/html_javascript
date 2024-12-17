41d67b2a5b58dadba7591876db2ca993c9fa3087
im-sadi-aslam <sadafaslam249@gmail.com>
2024-12-17 22:29:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+ans,1
+alert("Welcome to my website!");
+
+ans,2
+ document.write("Welcome to my website!<br>Enjoy your visit.<br>Have a great day!");
+
+ ans, 3;
+ alert("Welcome to my website!");
+ alert("Enjoy your visit.");
+ alert("Have a great day!");
+
+ ans,4
+ console.log("Welcome to my website!");
+console.log("Enjoy your visit.");
+console.log("Have a great day!");
+
+ans,5
+alert("Welcome to my project!");
+
+ans,6,a
+alert("Script in <head> section is running!");
+
+ans,6,b
+alert("Script in <body> section is running!");
+ans,6,c
+alert("This alert is inside the page's HTML content!");
+ans,6,d
+alert("This alert is placed after the page’s HTML!");
+
+                                  PDF=2;
+ans,1
+var username;
+ans,2
+var myName = "sadaf aslam";
+ans,3
+var message = "Hello World";
+alert(message);
+ans,4
+var studentName = "Sadaf Aslam";
+var studentAge = "20 years old";
+var studentCourse = "Web Development";
+
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Course: " + studentCourse);
+ans,5
+var alertMessage = "Congratulations!\nYou have successfully completed the task.";
+alert(alertMessage);
+ans,6
+var email = "example@example.com";
+alert("My email address is " + email);
+ans,7
+var book = "A smarter way to learn JavaScript";
+alert("I am learning from the book " + book);
+ans,8
+document.write("Yay! I can write HTML content through JavaScript.");
+ans,9
+var content = "This is an amazing JavaScript exercise!";
+alert(content);
+document.write(content);
+
